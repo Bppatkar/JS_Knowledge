4b20a6515126fa7401975b7a36138e2416c78473
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-07-31 15:29:38 +0530

    Leetcode 76. Minimum Window Substring

--- LOGIC_DSA_Learning/practice/rough_copy2.js
+++ LOGIC_DSA_Learning/practice/rough_copy2.js
@@ -37,6 +37,7 @@ function longestSubstringKDistinct(s, k) {
   let left = 0, right = 0, maxLength = 0, map = {};
   while (right < s.length) {
     map[s[right]] = (map[s[right]] || 0) + 1;
+    // when we use Object.keys(map).length , Time complexity is O(26 * n) for lowercase english letters
     while (Object.keys(map).length > k) {
       map[s[left]]--;
       if (map[s[left]] === 0) delete map[s[left]];
@@ -47,5 +48,74 @@ function longestSubstringKDistinct(s, k) {
   }
   return maxLength;
 }
-console.log("longestSubstringKDistinct", longestSubstringKDistinct("eceba", 2)); // 3
-console.log("longestSubstringKDistinct", longestSubstringKDistinct("aa", 1)); // 2
+//* TC - O(n) - right move onces till end and left move at most n times, and O(1) for map operations since we have only 26 lowercase english letters so total time complexity is O(n)+O(1) = O(n)
+//* SC - O(1) - map will have at most 26 lowercase english letters so space complexity is O(1) */
+
+// console.log("longestSubstringKDistinct", longestSubstringKDistinct("eceba", 2)); // 3
+// console.log("longestSubstringKDistinct", longestSubstringKDistinct("aa", 1)); // 2
+
+//---------------------------------------------
+
+//! Leetcode 76. Minimum Window Substring
+/* 
+//* The Complete Algorithm - 
+Create needMap ,Create windowMap, matched = 0 ,left = 0, bestWindow = Infinity
+
+For every Right
+1. Add current character into windowMap
+2. If current character exists in needMap
+   AND
+   window frequency == need frequency
+   matched++
+3. While window is valid
+      Update minimum answer
+      Remove left character
+      If removing breaks a requirement
+            matched--
+      left++
+4. Move right
+Finished.
+Return answer.
+*/
+//! MIMP  - 
+/* 
+remember one sentence. We never compare two HashMaps.
+We maintain a small variable (matched) that tells us whether all requirements are currently satisfied.
+*/
+
+function minWindow(s, t) {
+  if (s.length < t.length) return "";
+  let needMap = {}, windowMap = {};
+  for (let ch of t) {
+    needMap[ch] = (needMap[ch] || 0) + 1;
+  }
+  let left = 0, minLength = Infinity, matched = 0, startIndex = 0;
+  let required = Object.keys(needMap).length;
+
+  for (let right = 0; right < s.length; right++) {
+    let ch = s[right];
+    windowMap[ch] = (windowMap[ch] || 0) + 1;
+    if (needMap[ch] !== undefined && needMap[ch] === windowMap[ch]) {
+      matched++;
+    }
+    while (matched === required) {
+      if (right - left + 1 < minLength) {
+        minLength = right - left + 1;
+        startIndex = left;
+      }
+      let leftChar = s[left];
+      windowMap[leftChar]--;
+      if (needMap[leftChar] !== undefined && windowMap[leftChar] < needMap[leftChar]) {
+        matched--;
+      }
+      left++;
+    }
+  }
+  return minLength === Infinity ? "" : s.substring(startIndex, startIndex + minLength)
+}
+
+console.log("minWindow", minWindow("ADOBECODEBANC", "ABC")); // BANC
+console.log("minWindow", minWindow("a", "a")); // a
+console.log("minWindow", minWindow("a", "aa")); // ""
+
+

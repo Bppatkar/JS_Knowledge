2fdb93e5d44274e9d405e02c16e68dbcfbf71cf1
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2026-08-19 13:53:29 +0530

    solving last basic question of stack then we moved towards monotonic stack

--- LOGIC_DSA_Learning/practice/phase3.1_ADT.js
+++ LOGIC_DSA_Learning/practice/phase3.1_ADT.js
@@ -22,7 +22,7 @@ var isValid = function (s) {
 // console.log(isValid("()")); // true
 // console.log(isValid("()[]{}")); // true
 // console.log(isValid("(]")); // false
-s
+
 //! Leetcode 155. Min Stack
 
 var MinStack = function () {
@@ -120,3 +120,64 @@ MinStack.prototype.getMin = function () {
 // console.log(minStack.top());    // return 0
 // console.log(minStack.getMin()); // return -2
 
+//! Leetcode 150. Evaluate Reverse Polish Notation
+/* 
+my thinking in steps
+1- create stack and set for operators set = {"+", "-", "*", "/"}
+ 2- agar token mein operator hai to calculation perform result firse store in stack
+ 3 - agar operator nahi hai to means operand hai to string se number mein change krege and 
+ 4 - last 2 value pop krege and operator ke according calculation perform krege and result ko stack mein push krege
+ 5- last mein stack mein jo value bachi hai wahi answer hoga
+*/
+
+
+var evalRPN = function (tokens) {
+  let operators = new Set(["+", "-", "*", "/"]), stack = [], result;
+  for (let i = 0; i < tokens.length; i++) {
+    if (operators.has(tokens[i])) {
+      let val1 = stack.pop();
+      let val2 = stack.pop();
+      if (tokens[i] === "+")
+        result = val2 + val1;
+      else if (tokens[i] === "-")
+        result = val2 - val1;
+      else if (tokens[i] === "*")
+        result = val2 * val1;
+      else
+        result = Math.trunc(val2 / val1);
+      stack.push(result);
+    } else stack.push(Number(tokens[i]));
+  }
+  return stack[stack.length - 1];
+}
+// --------------------------------------------------------
+//! using MAP
+var evalRPN = function (tokens) {
+  let stack = [], map = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => Math.trunc(a / b),
+  }
+
+  for (let i = 0; i < tokens.length; i++) {
+    if (map[tokens[i]]) {
+      let b = stack.pop();
+      let a = stack.pop();
+      let result = map[tokens[i]](a, b);
+      stack.push(result);
+    } else stack.push(Number(tokens[i]))
+  }
+  return stack[0];
+}
+
+// console.log(evalRPN(["2", "1", "+", "3", "*"])); // 9
+// console.log(evalRPN(["4", "13", "5", "/", "+"])); // 6
+// console.log(evalRPN(["10", "6", "9", "3", "/", "-", "*"])); // 30
+// console.log(evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"])); // 22
+
+//! Leetcode 682. Baseball Game
+var calPoints = function (ops) { }
+console.log(calPoints(["5", "2", "C", "D", "+"])); // 30
+console.log(calPoints(["5", "-2", "4", "C", "D", "9", "+", "+"])); // 27
+console.log(calPoints(["1", "C"])); // 0
